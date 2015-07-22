ce1f87beb2f70049496727b8b7f50d261ab0847b
Anton Malkov <anton.malkov@gmail.com>
2015-07-22 18:43:11 +0200

    Replace user email with name in pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,14 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, :styles => { large: "600x600", medium: "300x300", thumb: "100x100" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+	validates :image, presence: true
+	validates :description, presence: true
+
+	validates :description, length: {
+    minimum: 5,
+    maximum: 250,
+    too_short: "must have at least %{count} characters",
+    too_long: "must have at most %{count} characters"
+  }
 end
